921c764285b8aca6d32c9ad6306a983904c5623a
Korawich22 <6531503002@lamduan.mfu.ac.th>
2023-09-05 16:30:25 +0700

    Finished

--- src/main/java/th/mfu/BMICalculatorServlet.java
+++ src/main/java/th/mfu/BMICalculatorServlet.java
@@ -9,20 +9,58 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 //TODO: add webservlet to "/calbmi"
+@WebServlet(urlPatterns = "/calbmi")
 public class BMICalculatorServlet extends HttpServlet{
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //TODO: get parameter from request: "weight" and "height"
+        String weightStr = request.getParameter("weight");
+        String heightStr = request.getParameter("height");
+        if (weightStr != null && !weightStr.isEmpty() && heightStr != null && !heightStr.isEmpty()) {
+            try{
+        double weight = Double.parseDouble(weightStr);
+        double height = Double.parseDouble(heightStr);
         
         //TODO: calculate bmi
+        
+        double bmi = Math.round(calculateBMI(weight,height));
+
 
         //TODO: determine the built from BMI
+        String built = determineBuilt(bmi);
       
         //TODO: add bmi and built to the request's attribute
+        request.setAttribute("bmi", bmi);
+        request.setAttribute("built", built);
 
         //TODO: forward to jsp
-           
+        request.getRequestDispatcher("/bmi_result.jsp").forward(request, response);
+            }catch (NumberFormatException e)
+            {
+                response.getWriter().println("Invalid weight or height value");
+            }
+        }
+    }
+    
+
+    private String determineBuilt(Double bmi) {
+        if(bmi<18.5){
+             return "underweight";
+        } else if(bmi>=18.5 && bmi<25){
+            return "normal";
+        } else if(bmi>=25 && bmi<30){
+            return "overweight";
+        } else if(bmi>=30 && bmi<35){
+            return "obese";
+        } else {
+            return "extremely obese";
+        }
+    }
+
+    private Double calculateBMI(Double weight, Double height) {
+
+        return weight / (height * height);
     }
     
 }
